b44efefab310d842df5a7509ec2db557c09a1a2d
Maximilien Rozniecki <mrozniec@z4r7p5.42lyon.fr>
2021-09-22 19:14:01 +0200

    todo check leaks

--- main.c
+++ main.c
@@ -3,14 +3,42 @@
 void child(char *cmd, char **envp)
 {
 	char **cmd_split;
-	char *path;
+	char **tmp_path;
+	char **path;
+	int i;
 
 	cmd_split = ft_split(cmd, ' ');
-	path = ft_strjoin("/bin/", cmd_split[0]);
-	if (cmd_split == NULL)
+	path = ft_split(envp[4] + 5, ':');
+	i = 0;
+	while (path[i])
+		i++;
+	tmp_path = malloc(sizeof(char *) * (i + 1));
+	if (!tmp_path)
+		return (perror("malloc"));
+	while (--i >= 0)
+	{
+		tmp_path[i] = ft_strjoin(path[i], "/");
+		free(path[i]);
+	}
+	while (tmp_path[++i])
+	{
+		path[i] = ft_strjoin(tmp_path[i], cmd_split[0]);
+		free(tmp_path[i]);
+	}
+	free(tmp_path);
+	if (cmd_split == NULL || path == NULL)
 		return (perror("malloc split"));
-	if (execve("/bin/", &cmd_split[1], envp) == -1)
-		return (perror("excecve error"));
+	i = -1;
+	while (path[++i])
+	{
+		perror(path[i]);
+		if (access(path[i], X_OK) >= 0)
+		{
+			execve(path[i], cmd_split, envp);
+			return;
+		}
+	}
+	return (perror("excecve error"));
 }
 
 void    pipex(int f1, int f2, char *cmd1, char *cmd2, char **envp)
@@ -39,7 +67,6 @@ void    pipex(int f1, int f2, char *cmd1, char *cmd2, char **envp)
 			return (perror("fork error"));
 		if (child2 == 0)
 		{
-			perror("hello");
 			dup2(f2, 1);
 			dup2(pipend[0], 0);
 			close(pipend[1]);
@@ -56,12 +83,17 @@ int main(int argc, char **argv, char **envp)
 {
 	int f1;
 	int f2;
-	f1 = open(argv[1], O_RDONLY);
-	f2 = open(argv[4], O_CREAT | O_RDWR | O_TRUNC, 0644);
-	if (f1 < 0 || f2 < 0)
-		return (-1);
-	pipex(f1, f2, argv[2], argv[3], envp);
-	close(f1);
-	close(f2);
-	return (0);
+
+	if (argc == 5)
+	{
+		f1 = open(argv[1], O_RDONLY);
+		f2 = open(argv[4], O_CREAT | O_RDWR | O_TRUNC, 0644);
+		if (f1 < 0 || f2 < 0)
+			return (-1);
+		pipex(f1, f2, argv[2], argv[3], envp);
+		close(f1);
+		close(f2);
+		return (0);
+	}
+	return (-1);
 }
